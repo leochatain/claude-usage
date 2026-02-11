06615c7f4b6ebd0634110addb04ac3bb2c0068a3
Leo Chatain <leochatain@gmail.com>
2026-02-11 11:33:16 +0000

    Initial Commit

---
+++ claude-usage/claude_usageApp.swift
@@ -0,0 +1,17 @@
+//
+//  claude_usageApp.swift
+//  claude-usage
+//
+//  Created by Leo on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct claude_usageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
